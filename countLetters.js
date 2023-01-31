bfdf97cc17bc068f35aad4dac8ba3aef8a9d4df4
Mike <mike.simnes@gmail.com>
2023-01-30 17:12:42 -0800

    first commit, added assertion functions, actual functions and some test code.

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// ASERTION FUNCTIONS
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  } else if (actual !== expected) {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+};
+
+// ACTUAL FUNCTION
+
+const countLetters = function(string) {
+  let results = {};
+  for (let letter of string) {
+    if (letter === " ") {
+      continue;
+    } else if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+
+console.log(countLetters("hahaha this is a test"));
