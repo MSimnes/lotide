9790fc545a7ad58b0c52795ba596bc6591af722c
Mike <mike.simnes@gmail.com>
2023-01-31 19:14:01 -0800

    first commit, assertion function eqArrays used, actual function checks if arrays, if objects and uses recursive function to check if not equal. some test code

---
+++ assertObjectsEqual.js
@@ -0,0 +1,49 @@
+// ASSERTION FUNCTIONS
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) return false;
+  }
+  return true;
+};
+
+
+const eqObjects = function(object1, object2) {
+  // store the keys in a variable for easy looping
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  // check if length are not equal
+  if (keys1.length !== keys2.length) return false;
+  for (let val of keys1) {
+    // check if values are arrays and check if not equal using eqArrays
+    if (Array.isArray(object1[val]) && Array.isArray(object2[val])) {
+      const arraysAreEqual = eqArrays(object1[val], object2[val]);
+      if (!arraysAreEqual) return false;
+    }
+    // use recursion to check if values are objects and if those objects are not equal
+    else if (typeof object1[val] === 'object' && typeof object2[val] === 'object') {
+      const objectsAreEqual = eqObjects(object1[val], object2[val]);
+      if (!objectsAreEqual) return false;
+    }
+    // check if the values are not equal
+    else {
+      if (object1[val] !== object2[val])
+        return false;
+    }
+  }
+  // return true if all checks passed
+  return true;
+};
+
+// ACTUAL FUNCTION
+
+const assertObjectsEqual = function(objectOne, objectTwo) {
+  const inspect = require('util').inspect;
+  const test = eqObjects(objectOne, objectTwo);
+  test === true ? console.log(`✔✔✔ Assertion Passed: ${inspect(objectOne)} === ${inspect(objectTwo)}`) : console.log(`🚨🚨🚨 Assertion Failed: ${inspect(objectOne)} !== ${inspect(objectTwo)}`);
+};
+
+// TEST CODE
+
+assertObjectsEqual({a:1}, {a:1});
