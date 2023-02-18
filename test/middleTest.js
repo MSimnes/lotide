89a186f9edbf240863ce7d608403001ac049c1c0
Mike <mike.simnes@gmail.com>
2023-02-18 15:36:12 -0800

    modified middleTest to use M & C, all working properly.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const middle =  require("../middle");
-// const eqArrays = require("../eqArrays");
-// TEST CODE
+const assert = require('chai').assert;
+const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle(arrayOdd), [3]);
+  });
+  it("return ['mid1', 'mid2'] for ['foo', 'bar', 'mid1', 'mid2', 'foobar', 'barfoo']", () => {
+    assert.deepEqual(middle(arrayEven), ['mid1', 'mid2']);
+  });
+  it("return [] for ['x', 'y'] ", () => {
+    assert.deepEqual(middle(['x', 'y']), []);
+  });
+});
+
+// TEST VARIABLES
 let arrayOdd = [1, 2, 3, 4, 5];
-let arrayEven = ['foo', 'bar', 'middle1', 'middle2', 'foobar', 'barfoo'];
-let arrayShort = ['x','y'];
-
-assertArraysEqual(middle(arrayOdd), [3]);
-assertArraysEqual(middle(arrayEven), ['middle1', 'middle2']);
-assertArraysEqual(middle(arrayShort), []);
+let arrayEven = ['foo', 'bar', 'mid1', 'mid2', 'foobar', 'barfoo'];
