15173a748d3ff1d62e54a1eabee43b64e63f810c
Mike <mike.simnes@gmail.com>
2023-02-18 15:05:51 -0800

    did npm init for lotide directory, edited json to include mocha and chai. modified headTest to use m + c for tests, deleted old test code. all working properly

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
-// TEST CODE
 
-assertEqual(head([1,2,3,4]),1);
-assertEqual(head([1,3]), 1);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for '[5]'", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+});
