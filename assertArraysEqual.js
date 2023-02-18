6b4ef2c4f6272056c94b9a462bbec3084ca6ccd5
Mike <mike.simnes@gmail.com>
2023-02-17 20:02:33 -0800

    create test file for assertArrasyEqual.js and cleaned out file. all working properly.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,8 @@
-// ASSERTION FUNCTION
-
-// function to check if two arrays are equal
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) return false;
-  }
-  return true;
-};
-
+const eqArrays = require("./eqArrays");
 // ACTUAL FUNCTION
-
 // function to display the appropriate message to the console if two arrays match or not leveraging existing eqArrays function.
-
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   let test = eqArrays(arrayOne, arrayTwo);
   test === true ? console.log(`✔✔✔ Assertion Passed: [${arrayOne}] === [${arrayTwo}]`) : console.log(`🚨🚨🚨 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`);
 };
-
-// TEST CODE
-
-assertArraysEqual([1,2,3,4],[1,2,3,4]);
-assertArraysEqual([2,3,4],[1,2,3,4]);
-
 module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+// TEST CODE
+
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([2,3,4],[1,2,3,4]);
