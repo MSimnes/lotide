6bc291b8efe0b6e8379164fb5ed6584c66986437
Mike <mike.simnes@gmail.com>
2023-02-18 18:22:41 -0800

    created test file and M & C tests for flatten, included some variables for testing purposes, cleaned up flatten.js, left commented out two previouly useful assertion functions for documentation.

---
+++ test/flattenTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const flatten = require('../flatten');
+
+describe("#eqArrays", () => {
+  it("returns [1, 2, 3, 4, 5] for [1, 2, [3, 4], 5]", () => {
+    assert.deepEqual(flatten(arrayOne), [1, 2, 3, 4, 5]);
+  });
+  it("returns [1, 2, 3, 4, 8, 9, 10, 5, 6] for [1, 2, [3], [4, [8,9,10], 5], 6, []]", () => {
+    assert.deepEqual(flatten(arrayThree), [1, 2, 3, 4, 8, 9, 10, 5, 6]);
+  });
+});
+// TEST VARIABLES
+const arrayOne = [1, 2, [3, 4], 5];
+const arrayTwo = [1, [], 2, 3, 4, [5]];
+const arrayThree = [1, 2, [3], [4, [8,9,10], 5], 6, []];
+
+
