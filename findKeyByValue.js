ca77ca0d039862ce5b1783d11345deafcc2f0002
Mike <mike.simnes@gmail.com>
2023-02-18 19:35:42 -0800

    Created test file and M & C tests for findKeyByValue, all working properly. Cleaned up findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,17 +1,3 @@
-// ASSERTION FUNCTIONS
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  } else if (actual !== expected) {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-};
-
-// ACTUAL FUNCTION
-
 const findKeyByValue = function(object, value) {
   for (const prop in object) {
     if (object[prop] === value) {
@@ -21,14 +7,16 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-// TEST CODE
+// ASSERTION FUNCTIONS
 
-const bestTVShowsByGenre = {
-  // eslint-disable-next-line camelcase
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+//     return;
+//   } else if (actual !== expected) {
+//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+//     return;
+//   }
+// };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
---
+++ test/findKeyBValueTest.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+
+describe("#findKeyByValue", () => {
+  it('returns The Wire for findKeyByValue({sci_fi: "The Expanse", comedy: "Brooklyn Nine-Nine", drama:  "The Wire"}, drama)', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+});
+
+// TEST VARIABLES
+
+const bestTVShowsByGenre = {sci_fi: "The Expanse", comedy: "Brooklyn Nine-Nine", drama:  "The Wire"};
