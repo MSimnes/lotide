2196a974f9c89b5e842a7a9f96ee1c4335c33025
Mike <mike.simnes@gmail.com>
2023-02-19 20:05:49 -0800

    removed assert functions and test code.

--- without.js
+++ without.js
@@ -5,32 +5,4 @@ const without = function(source, itemsToRemove) {
   return result;
 };
 
-// ASSERTION FUNCTIONS
-
-// function to check if two arrays are equal
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) return false;
-  }
-  return true;
-};
-
-// function to display the appropriate message to the console if two arrays match or not leveraging existing eqArrays function.
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let test = eqArrays(arrayOne, arrayTwo);
-  test === true ? console.log(`✔✔✔ Assertion Passed: ${arrayOne} === ${arrayTwo}`) : console.log(`🚨🚨🚨 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
-};
-
-// TEST CODE
-
-let words = ['hi', 'hello', 'bye', 'goodbye'];
-let remove = ['hi', 'bye'];
-//test the function
-assertArraysEqual(without(words, remove), ['hello', 'goodbye']);
-// test that the function doesn't mutate original array
-assertArraysEqual(words, ['hi', 'hello', 'bye', 'goodbye']);
-
 module.exports = without;
