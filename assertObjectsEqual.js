6ef3614ce1df6f094336eba2963f5838f03cc820
Mike <mike.simnes@gmail.com>
2023-02-19 18:47:29 -0800

    removed test code and one require

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,10 @@
-const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 
-// leverages eqObjects and eqArrays and returns a clear message to the console if they match or not.
+// leverages eqObjects and eqArrays and returns a clear message to the console if the objects match or not.
 const assertObjectsEqual = function(objectOne, objectTwo) {
   const inspect = require('util').inspect;
   const test = eqObjects(objectOne, objectTwo);
   test === true ? console.log(`✔✔✔ Assertion Passed: ${inspect(objectOne)} === ${inspect(objectTwo)}`) : console.log(`🚨🚨🚨 Assertion Failed: ${inspect(objectOne)} !== ${inspect(objectTwo)}`);
 };
-
-// TEST CODE
-
-assertObjectsEqual({a:1}, {a:1});
 
 module.exports = assertObjectsEqual;
