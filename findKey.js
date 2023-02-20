1a12925c933d31207bfe95ab22bda5d8602cb571
Mike <mike.simnes@gmail.com>
2023-02-19 19:21:32 -0800

    removed assert functions, moved test code to new test file

--- findKey.js
+++ findKey.js
@@ -8,28 +8,4 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-// ASSERTION FUNCTION
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  } else if (actual !== expected) {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-};
-
-// TEST CODE
-
-const object1 = {
-  "Blue Hill": {stars: 1},
-  "Akaleri": {stars: 3},
-  "noma": {stars: 2},
-  "elBulli": {stars: 3},
-  "Ora": {stars: 2},
-  "Akelarre": {stars: 3}
-};
-
-assertEqual(findKey(object1, x => x.stars === 2), 'noma');
 module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,13 @@
+
+// TEST CODE
+
+const object1 = {
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3}
+};
+
+assertEqual(findKey(object1, x => x.stars === 2), 'noma');
