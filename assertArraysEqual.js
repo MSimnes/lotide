0a2960b928ddc12edd087b59d1809103605dfe50
Mike <mike.simnes@gmail.com>
2023-02-20 11:53:04 -0800

    modified file with JSDoc comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,15 @@
 const eqArrays = require("./eqArrays");
 
-// function to display the appropriate message to the console if two arrays match or not leveraging existing eqArrays function.
+/**
+ * Function to assert whether two arrays are equal or not.
+ * @param {array} arrayOne - First array to compare.
+ * @param {array} arrayTwo - Second array to compare.
+ * @returns {string} - Displays the appropriate message to the console if two arrays match or not.
+ */
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   const test = eqArrays(arrayOne, arrayTwo);
   test === true ? console.log(`✔✔✔ Assertion Passed: [${arrayOne}] === [${arrayTwo}]`) : console.log(`🚨🚨🚨 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`);
 };
+
 module.exports = assertArraysEqual;
