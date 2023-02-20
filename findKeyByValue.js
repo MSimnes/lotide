946bf46f14150dc4d5c003d14ad9df0d32773e88
Mike <mike.simnes@gmail.com>
2023-02-20 12:36:10 -0800

    reformatted to include JSDoc Comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,10 @@
-// Finds the key of the corresponding value passed in as an argument.
+/**
+ * Finds the key of the corresponding value argument in the object argument.
+ * @param {Object} object - The object to search through.
+ * @param {*} value - The value to search for.
+ * @returns {(string|undefined)} - The key of the value if found, otherwise undefined.
+ */
+
 const findKeyByValue = function(object, value) {
   for (const prop in object) {
     if (object[prop] === value) {
