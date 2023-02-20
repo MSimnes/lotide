92fe5994127aec593e10e27761c8a6c27f23ef7d
Mike <mike.simnes@gmail.com>
2023-02-20 12:13:22 -0800

    reformatted to include JSDoc Comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,9 +1,17 @@
 const eqObjects = require('./eqObjects');
 
-// leverages eqObjects and eqArrays and returns a clear message to the console if the objects match or not.
+/**
+ * Function to assert whether two objects are equal or not.
+ * @param {object} objectOne - First object to compare.
+ * @param {object} objectTwo - Second object to compare.
+ * @returns {string} - Displays the appropriate message to the console if two objects match or not.
+ */
+
 const assertObjectsEqual = function(objectOne, objectTwo) {
+  // Require the 'util' module to enable the use of inspect() to output the actual and expected objects as strings
   const inspect = require('util').inspect;
   const test = eqObjects(objectOne, objectTwo);
   test === true ? console.log(`✔✔✔ Assertion Passed: ${inspect(objectOne)} === ${inspect(objectTwo)}`) : console.log(`🚨🚨🚨 Assertion Failed: ${inspect(objectOne)} !== ${inspect(objectTwo)}`);
 };
+
 module.exports = assertObjectsEqual;
