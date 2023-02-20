6390a7c5aca413d740270691034d46c39793979b
Mike <mike.simnes@gmail.com>
2023-02-20 12:52:27 -0800

    reformatted to include JSDoc Comments

--- without.js
+++ without.js
@@ -1,4 +1,11 @@
-// Function to return a subset of the first arg 'source' array, removing unwanted elements from the second arg 'itemsToRmemove'
+/**
+ * Returns a subset of the first argument, removing any elements from the second argument.
+ *
+ * @param {Array} source - The array to filter
+ * @param {Array} itemsToRemove - The items to remove from the source array
+ * @returns {Array} - A new array with the itemsToRemove removed
+ */
+
 const without = function(source, itemsToRemove) {
   let result;
   result = source.filter(elem => !itemsToRemove.includes(elem));
