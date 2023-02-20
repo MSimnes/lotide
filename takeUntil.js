85bd96614dfa40bf0b7b72faf2a43d36f3ebdbea
Mike <mike.simnes@gmail.com>
2023-02-20 12:50:55 -0800

    reformatted to include JSDoc Comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,10 +1,19 @@
-// Returns a slice of the array from the beginning until the callback returns a truthy value.
+/**
+ * Returns a slice of the array from the beginning until the callback returns a truthy value.
+ *
+ * @param {Array} array - The source array.
+ * @param {Function} callback - The function to test each element of the array.
+ * @returns {Array} - A new array containing elements from the beginning of the source array until the callback function returns a truthy value.
+ */
+
 const takeUntil = function(array, callback) {
   let result = [];
   for (const item of array) {
     if (!callback(item)) {
       result.push(item);
-    } else break;
+    } else {
+      break;
+    }
   }
   return result;
 };
