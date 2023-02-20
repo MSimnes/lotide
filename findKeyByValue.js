5827cd039709a84b03c5e0af058dd9e7b78bba21
Mike <mike.simnes@gmail.com>
2023-02-19 19:33:48 -0800

    removed assert functions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,16 +8,4 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-// ASSERTION FUNCTIONS
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-//     return;
-//   } else if (actual !== expected) {
-//     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-//     return;
-//   }
-// };
-
 module.exports = findKeyByValue;
--- flatten.js
+++ flatten.js
@@ -8,23 +8,4 @@ const getArrayDepth = (value) => {
   return Array.isArray(value) ? 1 + Math.max(0, ...value.map(getArrayDepth)) : 0;
 };
 
-// TEST ASSERTION FUNCTIONS
-
-// 1. function to check if two arrays are equal
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) return false;
-  }
-  return true;
-};
-
-// 2.  function to display the appropriate message to the console if two arrays match or not leveraging existing eqArrays function.
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let test = eqArrays(arrayOne, arrayTwo);
-  test === true ? console.log(`✔✔✔ Assertion Passed: [${arrayOne}] === [${arrayTwo}]`) : console.log(`🚨🚨🚨 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`);
-};
-
 module.exports = flatten;
