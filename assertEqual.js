adcca5ab0784140119b77bbbfb228d2251464850
Mike <mike.simnes@gmail.com>
2023-02-20 12:04:52 -0800

    reformatted to include JSDoc Comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,19 @@
-// Function to log a clear message to the console whether actual === expected or not.
+/**
+ * Function to log a clear message to the console whether actual === expected or not.
+ *
+ * @param {*} actual - The actual value to compare.
+ * @param {*} expected - The expected value to compare.
+ * @returns {undefined}
+ */
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
     return;
-  } else if (actual !== expected) {
+  } else {
     console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
     return;
   }
 };
+
 module.exports = assertEqual;
