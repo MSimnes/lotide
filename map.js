3060e7d3546a482b671f86ce5b022e03a7464ef2
Mike <mike.simnes@gmail.com>
2023-02-20 12:44:33 -0800

    reformatted to include JSDoc Comments

--- map.js
+++ map.js
@@ -1,4 +1,10 @@
-// Returns a new array based on the results of the callback function.
+/**
+ * Returns a new array based on the results of the callback function.
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} callback - The function to execute on each element.
+ * @returns {Array} - The new array with the results.
+ */
+
 const map = function(array, callback) {
   const results = [];
   for (const item of array) {
