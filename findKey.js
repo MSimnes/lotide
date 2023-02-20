c0ff123d06afed843731e13030c39f624977661c
Mike <mike.simnes@gmail.com>
2023-02-20 12:33:11 -0800

    reformatted to include JSDoc Comments

--- findKey.js
+++ findKey.js
@@ -1,4 +1,10 @@
-// Takes in an object and a callback, returns the key according to the callback passed in.
+/**
+ * Takes in an object and a callback function and returns the first key that produces a truthy value from the callback function.
+ * @param {Object} object - The object to search for the key.
+ * @param {Function} callback - The callback function that will be called on each value in the object.
+ * @returns {string|undefined} - The key that passes the callback function's truth test, or undefined if no key is found.
+ */
+
 const findKey = function(object, callback) {
   for (const item in object) {
     if (callback(object[item])) {
