111c43d4902204898455dd8bdde7b5cfae28a652
Mike <mike.simnes@gmail.com>
2023-02-20 12:26:19 -0800

    reformatted to include JSDoc Comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,10 @@
-// Function compares two arrays and returns true or false if they match or not.
+/**
+ * Function to compare two arrays and return true or false if they match or not.
+ * @param {array} arrayOne - First array to compare.
+ * @param {array} arrayTwo - Second array to compare.
+ * @returns {boolean} - True if the two arrays match, false otherwise.
+ */
+
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) return false;
   for (let i = 0; i < arrayOne.length; i++) {
