f1d1359a212a5114671088262574d20ba06466a2
Mike <mike.simnes@gmail.com>
2023-02-20 12:23:46 -0800

    reformatted to include JSDoc Comments

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,10 @@
-// This function takes in a collection of allItems and return counts for items selected for in itemsToCount.
+/**
+ * This function takes in an array of allItems and returns an object containg counts for items selected for in itemsToCount object.
+ * @param {Array} allItems - The collection of items to be counted.
+ * @param {Object} itemsToCount - The items to be counted.
+ * @returns {Object} - An object containing the counts of the items to be counted.
+ */
+
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
   for (const item of allItems) {
