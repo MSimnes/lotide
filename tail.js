a7614c00ddfff2b8e08161be00d12ce28d4af941
Mike <mike.simnes@gmail.com>
2023-02-20 12:59:00 -0800

    reformatted to include JSDoc Comments

--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
 /**
  * Returns a new array containing all elements except for the first one.
- * 
+ *
  * @param {Array} array - The array to process.
  * @returns {Array} A new array containing all elements except for the first one.
  */
